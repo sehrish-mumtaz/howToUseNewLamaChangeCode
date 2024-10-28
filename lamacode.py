dc21bea6af5d59fe155e761436bbbf17341cbdbf
sehrish-mumtaz <sehrish.mumtaz02@gmail.com>
2024-10-29 04:33:30 +0500

    Update lamacode.py

--- lamacode.py
+++ lamacode.py
@@ -1,7 +1,7 @@
 # howToUseNewLamaChangeCode (text to tet conversion)
 ### when we will use lama version that is train on 8 billion perameter data
-export GROQ_API_KEY=<your-api-key-here>   #######**** (first use this)****
-import os
+#######**** (first use this)****
+export GROQ_API_KEY=<your-api-key-here>
 
 from groq import Groq
 
@@ -19,7 +19,9 @@ chat_completion = client.chat.completions.create(
     model="llama3-8b-8192",
 )
 
-print(chat_completion.choices[0].message.content) ######3****( second than use this )****
+
+######3****( second than use this )****
+print(chat_completion.choices[0].message.content)
 ###### in case of wanting to use different lama version that is trained on more data like
 Gemma 7B
 Model ID: gemma-7b-it
