902a7486eb7f55e35d54b6bd5fb8b876d0f10448
sehrish-mumtaz <sehrish.mumtaz02@gmail.com>
2024-10-29 02:54:02 +0500

    Create ragSIr TAlha.py

---
+++ ragSIr TAlha.py
@@ -0,0 +1,2 @@
+# 1st prompt what is RAG (retrieval augmented generation) what are the main component of Rag? i am a beginner having no knowlegde about rag. act as an expert and give me answer according to it
+# prompt i want to built a simple RAg based application. give me complete code. i donot have access to paid APIs. use open source model for it.
